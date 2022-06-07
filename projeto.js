27235dfad9a23872a517a5c315aa3c37e65ee089
MiguelFLRubio <95933401+MiguelFLRubio@users.noreply.github.com>
2022-06-07 22:53:59 +0100

    Criar butões e secção das previsões dinamicamente

--- projeto.js
+++ projeto.js
@@ -11,6 +11,12 @@ function myFunction() {
   }
 }
 
+
+// Secção butões
+
+const sectionBtns = document.querySelector(".butoes #localidades")
+const sectionPrevisoes = document.querySelector(".previsoes")
+
 let xhr = new XMLHttpRequest();
 xhr.open( 'GET', "https://api.ipma.pt/open-data/distrits-islands.json");
 xhr.addEventListener('load', processaResposta );
@@ -23,12 +29,57 @@ function processaResposta( e ) {
     console.log( 'Chegou a resposta em', endTime - startTime , 'ms');
 
     let textoRespostaJSON = e.target.responseText;
-
     let objResposta = JSON.parse( textoRespostaJSON );
 
-    console.log( objResposta );
+    let localidades = objResposta.data;
 
-    let localidade = objResposta.data[0];
+    for (const localidade of localidades) {
 
-    console.log( localidade.local);
+      const btn = document.createElement("button")
+      btn.textContent = localidade.local
+      btn.setAttribute("data-distrito", localidade.globalIdLocal)
+
+      sectionBtns.appendChild(btn)
+      btn.addEventListener("click", mostrarDados)
+    }
 }
+
+function mostrarDados(e) {
+  const target = e.currentTarget
+  const idGlobalBtn = target.getAttribute("data-distrito")
+  sectionPrevisoes.innerHTML = ""
+
+  fetch(`http://api.ipma.pt/open-data/forecast/meteorology/cities/daily/${idGlobalBtn}.json`)
+    .then(res => res.json())
+    .then(data => {
+
+      const dados = data.data
+      for (const localidade of dados) {
+        const div = document.createElement("div")
+        const innerDiv = document.createElement("div")
+        const tMin = document.createElement("h2")
+        const tMax = document.createElement("h2")
+        const forecastDate = document.createElement("h2")
+
+        tMin.textContent = localidade.tMin
+        tMax.textContent = localidade.tMax
+        forecastDate.textContent = localidade.forecastDate
+
+        tMin.classList.add("tMin")
+        tMax.classList.add("tMax")
+        forecastDate.classList.add("forecastDate")
+
+        div.classList.add("teste")
+
+        sectionPrevisoes.appendChild(div)
+        innerDiv.appendChild(tMin)
+        innerDiv.appendChild(tMax)
+        div.appendChild(forecastDate)
+        div.appendChild(innerDiv)
+      } 
+
+
+
+    })
+}
+
